43c67d9aede7c579c801bfc38d0a3ee926b6be07
Austin Seipp <aseipp@pobox.com>
2013-05-05 04:18:40 -0500

    Copy top-level module to a Direct module.
    The direct module will provide the current interface. I'm going
    to make an exception-based interface the default.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

---
+++ src/Text/Search/Whistlepig/Direct.hs
@@ -0,0 +1,36 @@
+-- |
+-- Module      : Text.Search.Whistlepig.IO
+-- Copyright   : (c) Austin Seipp 2012
+-- License     : BSD3
+--
+-- Maintainer  : mad.one@gmail.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- This module provides a mid-level FFI binding to the Whistlepig
+-- search engine. It is different from the top-level interface
+-- in that it *returns* errors, and does not throw them.
+-- 
+module Text.Search.Whistlepig.Direct
+       ( -- * Whistlepig search index
+         -- $whistlepig
+         module Text.Search.Whistlepig.Index
+       , module Text.Search.Whistlepig.Query
+       , module Text.Search.Whistlepig.Entry
+         -- * Error type
+       , Error
+       ) where
+import Text.Search.Whistlepig.Index
+import Text.Search.Whistlepig.Query
+import Text.Search.Whistlepig.Entry
+import Text.Search.Whistlepig.Util (Error)
+
+-------------------------------------------------------------------------------
+-- Docs
+
+-- $whistlepig
+-- Whistlepig is a minimalist realtime full-text search index. Its goal is to
+-- be as small and maintainable as possible, while still remaining useful,
+-- performant and scalable to large corpora. If you want realtime full-text
+-- search without the frills, Whistlepig may be for you.
+--
